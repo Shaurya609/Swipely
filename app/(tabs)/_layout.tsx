931755816cc8a186453e7e32f36a58f65e2b4633
Shaurya609 <shaurya609@gmail.com>
2026-08-21 22:27:48 +0530

    fix: remove unsupported tab unmount option

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,6 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          unmountOnBlur: true,
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
